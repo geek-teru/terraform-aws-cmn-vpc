b56c74d817ddb8d9ecc3bfe6e090256efc7e75ed
teru <you@example.com>
2025-04-29 21:50:06 +0900

    update vpc module

--- terraform/modules/vpc/nat-gateway.tf
+++ terraform/modules/vpc/nat-gateway.tf
@@ -1,8 +1,8 @@
 resource "aws_eip" "nat" {
-  for_each = var.pub_subnet_config
+  for_each = var.priv_subnet_config
 
   tags = {
-    Name        = "${var.env_name}-${var.sys_name}-${var.pub_subnet_config[each.key].az}-nat"
+    Name        = "${var.env_name}-${var.sys_name}-${var.priv_subnet_config[each.key].az}-nat"
     Environment = var.env_name
   }
   depends_on = [
@@ -12,13 +12,13 @@ resource "aws_eip" "nat" {
 
 # NAT gateway for each public subnet on each availability zone.
 resource "aws_nat_gateway" "cmn-vpc-ngw" {
-  for_each = var.pub_subnet_config
+  for_each = var.priv_subnet_config
 
   allocation_id = aws_eip.nat[each.key].id
   subnet_id     = aws_subnet.pub_subnet[each.key].id
 
   tags = {
-    Name        = "${var.env_name}-${var.sys_name}-${var.pub_subnet_config[each.key].az}-ngw"
+    Name        = "${var.env_name}-${var.sys_name}-${var.priv_subnet_config[each.key].az}-ngw"
     Environment = var.env_name
   }
 
