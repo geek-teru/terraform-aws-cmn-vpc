3b475c4c2f4d5cf88ca171c9a83687ac5fa7746a
teru <you@example.com>
2025-05-02 23:20:08 +0900

    outputを変更

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -2,10 +2,14 @@ output "cmn_vpc" {
   value = aws_vpc.cmn-vpc
 }
 
-output "cmn_vpc_pub_subnets" {
-  value = aws_subnet.pub_subnet[*]
+output "cmn_vpc_pub_subnet_ids" {
+  value = [
+    for subnet in aws_subnet.pub_subnet : subnet.id
+  ]
 }
 
-output "cmn_vpc_priv_subnets" {
-  value = aws_subnet.priv_subnet[*]
+output "cmn_vpc_priv_subnet_ids" {
+  value = [
+    for subnet in aws_subnet.priv_subnet : subnet.id
+  ]
 }
